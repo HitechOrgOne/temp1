c39d48603ed6d3d777c677c4ffb84e43e6f2b574
mallavar9 <mallavar9@gmail.com>
2025-08-21 20:27:22 +0530

    2

--- PythonProject/e2e/conftest.py
+++ PythonProject/e2e/conftest.py
@@ -1,13 +1,5 @@
 import pytest
 
-def pytest_addoption(parser):
-    parser.addoption(
-        "--base-url",
-        action="store",
-        default="https://example.com",
-        help="Base URL for the application under test",
-    )
-
 @pytest.fixture(scope="session")
 def base_url(pytestconfig):
     return pytestconfig.getoption("base_url")
